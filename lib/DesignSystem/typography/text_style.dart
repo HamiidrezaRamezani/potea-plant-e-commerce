4ae56ad6a99b0d84a77992090697d94c8e43b4ab
hamidreza <hamiidreza0121@gmail.com>
2024-01-31 15:42:28 +0330

    add persian font and add typography extention

---
+++ lib/DesignSystem/typography/text_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TextStyle{
+  TextStyle._();
+
+  // Widget h1(String text , ){
+  //   return Text(
+  //     text,
+  //     style: TextStyle(
+  //
+  //     ),
+  //   );
+  // }
+}
